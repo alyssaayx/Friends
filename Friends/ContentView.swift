b3947cca6a674fcb21a8359437f19a6dafa1969c
Alyssa Aw Yong <alyssa.awyong@gmail.com>
2021-06-26 11:29:09 +0800

    Finished up everything, including friend profile view

--- Friends/ContentView.swift
+++ Friends/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var friends = [Friend(name: "Minghao",
+                          icon: "person.fill.checkmark",
+                          school: "Hwa Chong",
+                          slothImage: "sloth3"),
+                   Friend(name: "Yixing",
+                          icon: "bolt.heart.fill",
+                          school: "Raffles Junior College",
+                          slothImage: "sloth2"),
+                   Friend(name: "Dawn",
+                          icon: "zzz",
+                          school: "Temasek Junior College",
+                          slothImage: "sloth1")]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(friends) { friend in
+                NavigationLink(destination: FriendDetailView(friend: friend)) {
+                    Image(systemName: friend.icon)
+                    
+                    VStack(alignment: .leading) {
+                        Text(friend.name)
+                            .bold()
+                        Text(friend.school)
+                    }
+                }
+                .navigationTitle("Friends")
+            }
+        }
     }
-}
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+        
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
---
+++ Friends/Friend.swift
@@ -0,0 +1,20 @@
+//
+//  Friend.swift
+//  Friends
+//
+//  Created by rgs on 26/6/21.
+//
+
+import Foundation
+
+struct Friend: Identifiable {
+    
+    var id = UUID()
+    
+    var name: String
+       var icon: String
+       var school: String
+       
+       var slothImage: String
+    
+}
